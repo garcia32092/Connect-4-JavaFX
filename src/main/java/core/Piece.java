98491e469355d7ff3d50347709f93da57af9cc00
garcia32092 <pops32092@gmail.com>
2023-03-26 01:48:38 -0700

    Added comments and javadocs

--- src/main/java/core/Piece.java
+++ src/main/java/core/Piece.java
@@ -1,12 +1,35 @@
+/**
+ * 
+ * 2023SpringB-X-SER216-15142 / ASU Online
+ * Assignment: Project: Deliverable 2
+ * @author Randy Elias Garcia
+ * Description: A class representing a game piece in a Connect 4 game.
+ * 
+ * @version 1.0.0
+ * 
+ */
+
 package core;
 
 public class Piece {
+
+    /**
+     * The color of the game piece.
+     */
     private String color;
 
+    /**
+     * Gets the color of the game piece.
+     * @return The color of the game piece.
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Sets the color of the game piece.
+     * @param color The color to set for the game piece to.
+     */
     public void setColor(String color) {
         this.color = color;
     }
--- src/main/java/ui/Connect4TextConsole.java
+++ src/main/java/ui/Connect4TextConsole.java
@@ -1,5 +1,15 @@
+/**
+ * 2023SpringB-X-SER216-15142 / ASU Online
+ * Assignment: Project: Deliverable 2
+ * @author Randy Elias Garcia
+ * Description: The Connect4TextConsole class represents the text-based console interface for the Connect 4 game.
+ * 
+ * @version 1.0.0
+ */
+
 package ui;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import core.Connect4Logic;
@@ -8,18 +18,28 @@ public class Connect4TextConsole {
     private Connect4Logic game;
     private int currentPlayer;
 
+    /**
+     * Constructs a new Connect4TextConsole instance and initializes the game with a new Connect4Logic object.
+     */
     public Connect4TextConsole() {
         this.game = new Connect4Logic();
         this.currentPlayer = 1;
     }
 
+    /**
+     * Starts the Connect 4 game and prompts players to make their moves until the game is over.
+     */
     public void start() {
         boolean gameInProgress = true;
 
         System.out.println("Begin Game. ");
 
+        // Loop until game is over
         while (gameInProgress) {
+            // Print the current state of the game board
             game.print();
+
+            // Determine the color of the current player
             String color;
             if (currentPlayer == 1) {
                 color = " x ";
@@ -28,13 +48,23 @@ public class Connect4TextConsole {
                 color = " o ";
             }
     
-            System.out.println(getPlayerName(currentPlayer) + "-your turn. Choose a column number from 1-7.");
+            // Prompt the current player for their move
+            System.out.println(getPlayerName(currentPlayer) + " - your turn. Choose a column number from 1-7.");
 
+            // Get the desired column from the current player
             Scanner scanner = new Scanner(System.in);
-            int desiredCol = scanner.nextInt() - 1;
+            int desiredCol = -1;
+            try {
+                desiredCol = scanner.nextInt() - 1;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter an integer from 1-7.");
+            }
 
+            // Check if the current player's piece is added to the desired column
             boolean moveMade = game.addPiece(desiredCol, color);
 
+            // Once a move has been successfully made, check if the game has been won or drawn, and end the game if necessary
             if (moveMade) {
                 if (game.checkForWin(desiredCol, color)) {
                     game.print();
@@ -51,6 +81,7 @@ public class Connect4TextConsole {
                     gameInProgress = false;
                     System.out.println("This game ends in a draw.");
                 }
+                // Switch to the next player's turn
                 currentPlayer = getOpponent(currentPlayer);
             }
 
@@ -58,14 +89,31 @@ public class Connect4TextConsole {
 
     }
 
+    /**
+     * Returns the name of the player associated with the given player number.
+     * 
+     * @param player the player number (1 or 2)
+     * @return the name of the player
+     */
     private String getPlayerName(int player) {
-        return currentPlayer == 1 ? "PlayerX" : "PlayerO";
+        return currentPlayer == 1 ? "Player X" : "Player O";
     }
     
+    /**
+     * Returns the opponent of the given player.
+     * 
+     * @param player the player number (1 or 2)
+     * @return the opponent's player number (2 or 1)
+     */
     private int getOpponent(int player) {
         return currentPlayer == 1 ? 2 : 1;
     }
 
+    /**
+     * The main method to start the Connect 4 game.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Connect4TextConsole gameConsole = new Connect4TextConsole();
         gameConsole.start();
